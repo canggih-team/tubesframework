0a558c729be1a449e49ee50a65b427aff8a4dedb
Zulfa Azka <zulfaazka354@yahoo.com>
2024-06-05 21:40:12 +0700

    zulfa

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="d-flex align-items-stretch">
+    <na id="sidebar">
+        </nav>
+        <div class="sidebar-header d-flex align-items-center">
+            <div class="avatar"><img src="{{ asset('img/avatar-6.jpg') }}" alt="..." class="img-fluid rounded-circle">
+            </div>
+            <div class="title">
+                <h1 class="h5">Kelompok 8</h1>
+                <p>Tubes Framework</p>
+            </div>
+        </div>
+        <span class="heading">Main</span>
+        >
+        <ul class="list-unstyled">
+            <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
+
+            <li>
+            <a href="{{url}}"> <i class="icon-grid"></i>
+                Category
+            </a>
+
+        </li>
+
+            <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example Dropddown </a>
+                <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                    <li><a href="#">Page</a></li>
+                </ul>
+            </li>
+
+        </ul>
+        </nav>
